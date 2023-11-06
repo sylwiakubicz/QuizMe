b180c1d81be895279619a32c54f1155c9f7ecfac
sylwiakubicz <sylwiakubicz31@gmail.com>
2023-11-06 20:24:39 +0100

    Revert "Install jquery"
    This reverts commit dfca6b55c0984ccc3c39c215ed2ca379ecf9c37c.
    
    Errors after instalation

--- quizme/src/compontens/Header.js
+++ quizme/src/compontens/Header.js
@@ -1,35 +1,9 @@
 import React from "react"
 import "../style.css"
-import $ from 'jquery';
 
 function Header() {
-
-    // $(function(){}) == $(document).ready(function() { ... }); - czyli czekamy aż strona się załaduje zanim wykonamy jakiekolwiem skrypty
-    $(function() { 
-        // Funkcja dodająca lub usuwająca klase z navbar w zależności od położenia scrolla
-        function handleScroll() {
-            if (document.documentElement.scrollTop <= 40) {
-                if ($(".nav-container ").hasClass("navbar-scrolled")) {
-                    $(".nav-container ").removeClass("navbar-scrolled")
-                  }
-                console.log("<=40")
-
-            } else {
-                $(".nav-container").addClass("navbar-scrolled")
-                console.log("more than 40")
-            }
-        } 
-                
-        // wywołanie przy pierwszym uruchomieniu strony
-        handleScroll()
-
-        // // wywoływanie funkcji scroll za każdym razem, gdy scroll zmieni pozycje 
-        // $(window).on("scroll", function()
-        //     { setTimeout (handleScroll, 400) } )                
-    })
-
     return (
-        <nav className="navbar" onScroll={handleScroll}>
+        <nav className="navbar">
             <div className="nav--container">
                 <div className="nav--logo_container">
                     <a className="nav--logo" href="#">
