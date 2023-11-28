f4ee48b194484bdaef760ad1ca75d1654e84c174
sylwiakubicz <sylwiakubicz31@gmail.com>
2023-11-28 19:49:19 +0100

    Przerobienie formularza do kontaktu pod Reacta

--- quizme/src/compontens/ContactUs.js
+++ quizme/src/compontens/ContactUs.js
@@ -1,25 +1,60 @@
 import React from "react"
 import "../style.css"
 
+const [contactFormData, setContanctFormData] = React.useState(
+    {
+        email: "",
+        title: "",
+        mwssage: ""
+    }
+)
+
+function handleChange(event) {
+    const {name, type, value} = event.target
+    setContanctFormData(prevData => {
+        return {
+            ...prevData,
+            [name]: value
+        }
+    })
+}
 
 function ContactUs() {
     return (
         <div class="form" id="contact-form">
             <div class="wrapper">
-                <form action="">
+                <form >
                     <h1>Contact Us</h1>
                     <div class="input-box">
-                        <input type="text" name="email" placeholder="Email" required />
+                        <input 
+                            type="email" 
+                            name="email" 
+                            placeholder="Email"
+                            onChange={handleChange}
+                            required 
+                        />
                         <i class="fa-solid fa-user"></i>
                     </div>
                     <div class="input-box">
-                        <input type="text" name="title" placeholder="Title" required />
+                        <input 
+                            type="text" 
+                            name="title" 
+                            placeholder="Title" 
+                            onChange={handleChange}
+                            required 
+                        />
                         <i class="fa-solid fa-envelope"></i>
                     </div>
                     <div class="textarea-box">
-                        <textarea form="contact-form" name="message" placeholder="Message..." maxlength="3000" required></textarea>
+                        <textarea 
+                            form="contact-form" 
+                            name="message" 
+                            placeholder="Message..." 
+                            maxlength="3000" 
+                            onChange={handleChange}
+                            required></textarea>
                     </div>
-                    <button type="submit" class="btn">Send Message</button>
+                    <button class="btn">Send Message</button>
                 </form>
             </div>
         </div>
