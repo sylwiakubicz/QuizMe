36150b15d8637f83d2573feb7b1b3dc5c4912d9b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-03-06 19:51:46 +0100

    Feat: Utworzono bardzo prostą templatkę do wysyłania potwierdzenia otrzymania wiadomosci.

--- api_quizme/controllers/sendMailControllers.js
+++ api_quizme/controllers/sendMailControllers.js
@@ -14,13 +14,31 @@ export const sendMail = (req, res) => {
         from: req.body.email,
         to: "quizme657@gmail.com",
         subject: req.body.title,
-        text: "from: " + req.body.email + "\n\n" + req.body.message
+        text: "from: " + req.body.email + "\n\n" + req.body.message,
+        html: `<div>
+                    <h1>from: ${req.body.email}</h1>
+                    <hr />
+                    <p>\n\n ${req.body.message} </p>
+                </div >`
+    }
+
+    const msgToUser = {
+        from: "quizme657@gmail.com",
+        to: req.body.email,
+        subject: "Your email was received",
+        text: "",
+        html: {path: "./emailTemplates/MessageReceivedTemplate.html"}
     }
 
     transporter.sendMail(msg, (error, info) => {
         if (error) {
             console.log(error)
         }
+        transporter.sendMail(msgToUser, (error, info) => {
+            if (error) {
+                console.log(error)
+            }
+        })
         return res.status(200).json("Message sent!")
     })
 }
