09c0f34d45cea101d3e6f37debf9cd76724dce3b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-03-24 11:04:47 +0100

    feat: Wartości stanów title i active przechowywano w local storage,
    Zapobiega to nieproszonego przełączaniu pomiędzy podstronami tworzenia quizu w przypadku np odświeżenia karty.

--- quizme/src/compontens/pages/CreateQuiz.js
+++ quizme/src/compontens/pages/CreateQuiz.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import AddQuestion from "../AddQuestion";
 
 import "../../styles/AddQuiz.css"
@@ -9,9 +9,17 @@ import Activate from "../Activate"
 
 export default function CreateQuiz() {
 
-    const [active, setActive] = React.useState("")
-    const [title, setTitle] = React.useState("")
+    const [active, setActive] = React.useState(JSON.parse(window.localStorage.getItem('active')) || "")
+    const [title, setTitle] = React.useState(JSON.parse(window.localStorage.getItem('quizTitle')) || "")
     const [error, setError] = React.useState("")
+
+    useEffect(() => {
+        localStorage.setItem('quizTitle', JSON.stringify(title));
+    }, [title]);
+
+    useEffect(() => {
+        localStorage.setItem('active', JSON.stringify(active));
+    }, [active]);
 
     return(
         <div className="myaccount-container page">
