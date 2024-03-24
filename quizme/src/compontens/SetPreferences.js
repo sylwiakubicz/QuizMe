7b5a267a5a4ba532277129687dd7f8bde07def43
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-03-24 11:14:54 +0100

    style: Naprawiono przycsik prevoius dla setPreferences, aby przekierowaywał na poprawną podstronę - AddQuestion

--- quizme/src/compontens/SetPreferences.js
+++ quizme/src/compontens/SetPreferences.js
@@ -7,7 +7,7 @@ export default function setPreferences(props) {
         <div className={props.active === "setPreferences" ? "settings-container" : "notShow"}>
             <button className="quiz--btn delete-btn" onClick={(e) => {
                     e.preventDefault()
-                    props.setActive("")
+                    props.setActive("addQuestion")
                     }}>Previous</button>
                 <button className="quiz--btn delete-btn" onClick={(e) => {
                     e.preventDefault()
