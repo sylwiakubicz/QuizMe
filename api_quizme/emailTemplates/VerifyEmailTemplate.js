6da9948b99349137b54d051d3e906d5b1ec05a99
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-04-08 19:57:41 +0200

    Chore: Utworzono templatkę maila do weryfikowania konta.

---
+++ api_quizme/emailTemplates/VerifyEmailTemplate.js
@@ -0,0 +1,61 @@
+const verifyEmailTemplate = (verificationCode) => {
+    
+`<html>
+    <head>
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/d49fca0d9e.js" crossorigin="anonymous"></script>
+        <style>
+            :root {
+                --primary-color: 31, 201, 130;
+                --text-dark: 0,0,0;
+            }
+
+            [data-theme='dark'] {
+                --primary-color: 85,110,83;
+                --text-dark: 209, 212, 201;
+            }
+
+            * {
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+            }
+
+            a {
+                text-decoration: none;
+                color: rgb(var(--primary-color));
+            }
+
+            .nav--logo {
+                font-weight: bold;
+                font-size: 2.5rem;
+                font-family: 'Yanone Kaffeesatz', sans-serif;
+            }
+
+            .message--container {
+                text-align: center;
+                color: rgb(var(--text-dark))
+            }
+
+            .message--header {
+                padding-top: 3rem;
+                padding-bottom: 1rem;
+            }
+        </style>
+    </head>
+    <body data-theme="">
+        <div class="message--container">
+            <a class="nav--logo" href="http://localhost:3000/">
+                QuizMe
+            </a>
+            <h1 class="message--header">Please verify your account email</h1>
+            <p class="message--text">After verifing the email you will be able to sign in and create your own custom quizes</p>
+            <p class="message--text">Your verification code: </p>
+            <p class="message--header">${verificationCode}</p>
+        </div>
+    </body>
+</html>`
+}
+
+
+export {newsLetterEmail, welcomeEmail}
