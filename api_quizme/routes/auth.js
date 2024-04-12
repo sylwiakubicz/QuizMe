8e9e9948d81e5743c320c15359ef570cd07bc2b7
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-04-12 13:50:03 +0200

    Chore: Przeniesiono router do ponownego wysyłania kodu weryfikacyjnego do auth routers a w mail routes dodano nowy router do wysyłania maila o reset hasła.

--- api_quizme/routes/auth.js
+++ api_quizme/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {login, register, logout, changePassword, deleteAccount, checkIfHuman, updateUserData, verifyEmail} from "../controllers/authControllers.js"
+import {login, register, logout, changePassword, deleteAccount, checkIfHuman, updateUserData, verifyEmail, sendNewVerificationEmail} from "../controllers/authControllers.js"
 
 const router = express.Router()
 
@@ -8,6 +8,7 @@ router.post("/verify", checkIfHuman)
 router.post("/login", login)
 router.post("/logout", logout)
 router.post("/verify/email", verifyEmail)
+router.post("/send/verificationMail/new", sendNewVerificationEmail) 
 router.delete("/deleteAccount", deleteAccount)
 router.put("/changePassword", changePassword)
 router.put("/updateuserdata", updateUserData)
--- api_quizme/routes/mail.js
+++ api_quizme/routes/mail.js
@@ -1,11 +1,9 @@
 import express from "express"
-import {sendMail, sendVerificationMail} from "../controllers/sendMailControllers.js"
-import { sendNewVerificationEmail } from "../controllers/authControllers.js"
+import {sendMail, sendVerificationMail, sendResetPasswordMail} from "../controllers/sendMailControllers.js"
 
 const router = express.Router()
 
 router.post("/send", sendMail)
 router.post("/send/veryficationMail", sendVerificationMail)
-router.post("/send/verificationMail/new", sendNewVerificationEmail)
-
+router.post("/send/resetpasswordMail/code", sendResetPasswordMail)
 export default router
